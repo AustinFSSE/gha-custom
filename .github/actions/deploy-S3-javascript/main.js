aeaf80cadafbf7d7e579deb7510ed7299d4d4df8
AustinFSSE <alm2016.in@gmail.com>
2025-08-13 19:29:05 -0400

    added javascript custom action to deploy runner

--- .github/actions/deploy-S3-javascript/main.js
+++ .github/actions/deploy-S3-javascript/main.js
@@ -1,6 +1,10 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const exec = require("@actions/exec");
+
 async function run() {
   // Your code to deploy to S3 goes here
-  
+  core.notice('Hello from my custom javascript action!');
 }
 
 run();
